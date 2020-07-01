1d49fe15faab522c2db420b6c26edb389f6e0b3b
themylogin <themylogin@gmail.com>
2020-07-01 23:21:42 +0200

    Move populating /conf/base to truenas post-install script

--- scripts/finalize_rootfs.py
+++ scripts/finalize_rootfs.py
@@ -5,9 +5,3 @@ import sys
 
 if __name__ == "__main__":
     rootfs, = sys.argv[1:]
-
-    for file in ["group", "passwd", "syslog-ng/syslog-ng.conf"]:
-        original = f"{rootfs}/etc/{file}"
-        destination = f"{rootfs}/conf/base/etc/{file}"
-        os.makedirs(os.path.dirname(destination), exist_ok=True)
-        shutil.copy(f"{rootfs}/etc/{file}", destination)
