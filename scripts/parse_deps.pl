622038bbdad8ae30af89a1d3cf69bcd4d707c73b
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-04-09 23:10:00 +0500

    Always retrieve source/binary packages deps

--- scripts/parse_deps.pl
+++ scripts/parse_deps.pl
@@ -6,26 +6,21 @@ use Dpkg::Control::Info;
 use Dpkg::Deps;
 use JSON;
 
-my ($path, $depends_type) = @ARGV;
+my ($path) = @ARGV;
 my $control = Dpkg::Control::Info->new($path);
-if ($depends_type eq 'Build-Depends'){
-    my $fields = $control->get_source();
-    my $build_depends = deps_parse($fields->{'Build-Depends'});
-    my %package_info = ('name'=> $fields->{'Source'}, 'build_depends'=> deps_concat($build_depends));
-    my $json_str = encode_json \%package_info;
-    print("$json_str");
-} elsif ($depends_type eq 'Depends') {
-    my @packages = $control->get_packages();
-    my @packages_list;
-    foreach my $package (@packages)
-    {
-        push(@packages_list, {
-            'name' => $package->{'Package'},
-            'depends' => $package->{'Depends'}
-        });
-    }
-    my $json_str = encode_json(\@packages_list);
-    print("$json_str");
-} else {
-    die "Unrecognized depends_type specified";
+my $fields = $control->get_source();
+my $build_depends = deps_parse($fields->{'Build-Depends'});
+my %src_package_info = ('name'=> $fields->{'Source'}, 'build_depends'=> deps_concat($build_depends));
+
+my @packages = $control->get_packages();
+my @packages_list;
+foreach my $package (@packages)
+{
+    push(@packages_list, {
+        'name' => $package->{'Package'},
+        'depends' => $package->{'Depends'}
+    });
 }
+my %package_info = ('source_package'=> \%src_package_info, 'binary_packages'=> \@packages_list);
+my $json_str = encode_json \%package_info;
+print("$json_str");
