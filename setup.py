1dd2e64ade9dde71f522ac8259d6e4d68ec60c5f
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-04-13 14:05:37 +0500

    Add setup file for install scale-build

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    'pyyaml',
+]
+
+
+setup(
+    name='scale_build',
+    description='A build framework for TrueNAS SCALE',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD',
+    platforms='any',
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'scale_build = scale_build.main:main',
+        ],
+    },
+)
