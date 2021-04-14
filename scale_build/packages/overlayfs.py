874440d526c01cbdc7d3c9d4b14c500b86160bc8
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-04-14 05:57:07 +0500

    Implement make overlayfs

--- scale_build/packages/overlayfs.py
+++ scale_build/packages/overlayfs.py
@@ -1,8 +1,40 @@
 import os
 import shutil
 
+from scale_build.exceptions import CallError
 from scale_build.utils.run import run
-from scale_build.utils.variables import CHROOT_OVERLAY, DPKG_OVERLAY, WORKDIR_OVERLAY
+from scale_build.utils.variables import (
+    CACHE_DIR, CHROOT_BASEDIR, CHROOT_OVERLAY, DPKG_OVERLAY, PKG_DIR, WORKDIR_OVERLAY
+)
+
+
+def make_overlay_fs():
+    for path in (CHROOT_OVERLAY, DPKG_OVERLAY, WORKDIR_OVERLAY):
+        os.makedirs(path, exist_ok=True)
+
+    dpkg_overlay_packages = os.path.join(DPKG_OVERLAY, 'packages')
+
+    for entry in (
+        ([
+             'mount', '-t', 'overlay', '-o',
+             f'lowerdir={CHROOT_BASEDIR},upperdir={CHROOT_OVERLAY},workdir={WORKDIR_OVERLAY}',
+             'none', f'{DPKG_OVERLAY}/'
+        ], 'Failed overlayfs'),
+        (['mount', 'proc', os.path.join(DPKG_OVERLAY, 'proc'), '-t', 'proc'], 'Failed mount proc'),
+        (['mount', 'sysfs', os.path.join(DPKG_OVERLAY, 'sys'), '-t', 'sysfs'], 'Failed mount sysfs'),
+        (['mount', '--bind', PKG_DIR, dpkg_overlay_packages], 'Failed mount --bind /packages', dpkg_overlay_packages),
+        (
+            ['mount', '--bind', os.path.join(CACHE_DIR, 'apt'), os.path.join(DPKG_OVERLAY, 'var/cache/apt')],
+            'Failed mount --bind /var/cache/apt',
+        ),
+    ):
+        if len(entry) == 2:
+            command, msg = entry
+        else:
+            command, msg, path = entry
+            os.makedirs(path, exist_ok=True)
+
+        run(command, exception=CallError, exception_msg=msg)
 
 
 def remove_overlay_fs():
