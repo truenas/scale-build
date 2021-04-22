de830475a294428a64b6f56d32c7f227320a0dc5
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-04-22 05:52:30 +0500

    Use bootstrapdir class to manage bootstrap dirs

---
+++ scale_build/bootstrap/bootstrapdir.py
@@ -0,0 +1,143 @@
+import os
+import shutil
+
+from scale_build.clean import clean_packages
+from scale_build.utils.paths import BUILDER_DIR, CHROOT_BASEDIR
+from scale_build.utils.run import run
+
+from .cache import CacheMixin
+from .hash import HashMixin
+from .utils import get_apt_preferences, get_manifest
+
+
+class BootstrapDir(CacheMixin, HashMixin):
+
+    def __init__(self, logger):
+        self.logger = logger
+        self.chroot_basedir = CHROOT_BASEDIR
+
+    def setup(self):
+        if self.mirror_cache_intact:
+            # Mirror cache is intact, we do not need to re-create the bootstrap directory
+            self.logger.debug('Basechroot cache is intact and does not need to be changed')
+            return
+
+        self.add_trusted_apt_key()
+        apt_repos = get_manifest()['apt-repos']
+        self.run(
+            ['debootstrap'] + self.deopts + [
+                '--keyring', '/etc/apt/trusted.gpg.d/debian-archive-truenas-automatic.gpg', 'bullseye',
+                CHROOT_BASEDIR, apt_repos['url']
+            ]
+        )
+        self.setup_mounts()
+        if self.extra_packages_to_install:
+            self.run(['chroot', self.chroot_basedir, 'apt', 'install', 'y'] + self.extra_packages_to_install)
+
+        installed_packages = self.get_packages()
+
+        self.after_extra_packages_installation_steps()
+
+        # Save the correct repo in sources.list
+        apt_path = os.path.join(self.chroot_basedir, 'etc/apt')
+        apt_sources_path = os.path.join(apt_path, 'sources.list')
+        apt_sources = [f'deb {apt_repos["url"]} {apt_repos["distribution"]} {apt_repos["components"]}']
+
+        # Set bullseye repo as the priority
+        with open(os.path.join(apt_path, 'preferences'), 'w') as f:
+            f.write(get_apt_preferences())
+
+        # Add additional repos
+        for repo in apt_repos['additional']:
+            self.logger.debug('Adding additional repo: %r', repo['url'])
+            shutil.copy(os.path.join(BUILDER_DIR, repo['key']), os.path.join(self.chroot_basedir, 'apt.key'))
+            self.run(['chroot', self.chroot_basedir, 'apt-key', 'add', '/apt.key'])
+            os.unlink(os.path.join(self.chroot_basedir, 'apt.key'))
+            apt_sources.append(f'deb {repo["url"]} {repo["distribution"]} {repo["component"]}')
+
+        with open(apt_sources_path, 'w') as f:
+            f.write('\n'.join(apt_sources))
+
+        # Update apt
+        self.run(['chroot', self.chroot_basedir, 'apt', 'update'])
+
+        # Put our local package up at the top of the food chain
+        apt_sources.insert(0, 'deb [trusted=yes] file:/packages /')
+        with open(apt_sources_path, 'w') as f:
+            f.write('\n'.join(apt_sources))
+
+        self.clean_mounts()
+        self.save_build_cache(installed_packages)
+
+    def after_extra_packages_installation_steps(self):
+        pass
+
+    def add_trusted_apt_key(self):
+        self.run([
+            'apt-key', '--keyring', '/etc/apt/trusted.gpg.d/debian-archive-truenas-automatic.gpg', 'add',
+            os.path.join(BUILDER_DIR, 'keys/truenas.gpg')
+        ])
+
+    def run(self, *args, **kwargs):
+        return run(*args, logger=self.logger, **kwargs)
+
+    @property
+    def extra_packages_to_install(self):
+        raise NotImplementedError
+
+    @property
+    def deopts(self):
+        raise NotImplementedError
+
+    def setup_mounts(self):
+        self.run(['mount', 'proc', os.path.join(self.chroot_basedir, 'proc'), '-t', 'proc'])
+        self.run(['mount', 'sysfs', os.path.join(self.chroot_basedir, 'sys'), '-t', 'sysfs'])
+
+    def clean_mounts(self):
+        for command in (
+            ['umount', '-f', os.path.join(CHROOT_BASEDIR, 'proc')],
+            ['umount', '-f', os.path.join(CHROOT_BASEDIR, 'sys')],
+        ):
+            self.run(command, check=False)
+
+        shutil.rmtree(self.chroot_basedir)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.clean_mounts()
+
+
+class PackageBootstrapDirectory(BootstrapDir):
+
+    @property
+    def deopts(self):
+        return []
+
+    @property
+    def extra_packages_to_install(self):
+        return ['build-essential', 'dh-make', 'devscripts', 'fakeroot']
+
+    @property
+    def cache_filename(self):
+        return 'basechroot-package.squashfs'
+
+    def after_extra_packages_installation_steps(self):
+        if self.installed_packages_in_cache_changed:
+            clean_packages()
+
+
+class CdromBootstrapDirectory(BootstrapDir):
+
+    @property
+    def deopts(self):
+        return ['--components=main,contrib,nonfree', '--variant=minbase', '--include=systemd-sysv,gnupg']
+
+    @property
+    def extra_packages_to_install(self):
+        return []
+
+    @property
+    def cache_filename(self):
+        return 'basechroot-cdrom.squashfs'
--- scale_build/bootstrap/cache.py
+++ scale_build/bootstrap/cache.py
@@ -1,19 +1,22 @@
+import json
 import os
 
 from scale_build.clean import clean_packages
 from scale_build.exceptions import CallError
 
-from .hash import get_all_repo_hash, get_base_hash
+from .hash import get_all_repo_hash
 from .logger import get_logger
 from .utils import CACHE_DIR, CHROOT_BASEDIR, get_cache_filename, get_cache_hash_filename, HASH_DIR, run
 
 
 def create_basehash(cache_type):
+    # This is to check if apt mirrors have changed
     with open(os.path.join(CACHE_DIR, get_cache_hash_filename(cache_type)), 'w') as f:
         f.write(get_all_repo_hash())
 
 
 def check_basechroot_changed():
+    # This is for checking if we should clean packages
     logger = get_logger('package')
     base_hash = get_base_hash()
     basechroot_hash_path = os.path.join(HASH_DIR, '.basechroot.hash')
@@ -38,7 +41,7 @@ def save_build_cache(cache_type):
 
 def remove_basecache(cache_type):
     logger = get_logger(cache_type)
-    logger.debug('Removing base chroot cache for %s', cache_type)
+    logger.debug('Removing base chroot cache for %s', cache_type.name)
     for path in map(
         lambda p: os.path.join(CACHE_DIR, p), (get_cache_filename(cache_type), get_cache_hash_filename(cache_type))
     ):
@@ -71,3 +74,64 @@ def validate_basecache(cache_type):
             invalidated = False
 
     return not invalidated
+
+
+class CacheMixin:
+
+    @property
+    def cache_filename(self):
+        raise NotImplementedError
+
+    @property
+    def cache_file_path(self):
+        return os.path.join(CACHE_DIR, self.cache_filename)
+
+    @property
+    def cache_exists(self):
+        return all(
+            os.path.exists(p) for p in (self.cache_file_path, self.saved_packages_file_path, self.cache_hash_file_path)
+        )
+
+    def remove_cache(self):
+        for path in filter(
+            lambda p: os.path.exists(p),
+            (self.cache_file_path, self.saved_packages_file_path, self.cache_hash_file_path)
+        ):
+            os.unlink(path)
+
+    def get_mirror_cache(self):
+        if self.cache_exists:
+            with open(self.cache_hash_file_path, 'r') as f:
+                return f.read().strip()
+
+    def save_build_cache(self, installed_packages):
+        self.logger.debug('Caching CHROOT_BASEDIR for future runs...')
+        self.run(['mksquashfs', self.chroot_basedir, self.cache_filename])
+        self.update_saved_packages_list(installed_packages)
+        self.update_mirror_cache()
+
+    @property
+    def mirror_cache_intact(self):
+        intact = True
+        if not self.cache_exists:
+            # No hash file? Lets remove to be safe
+            intact = False
+            self.logger.debug('Cache does not exist')
+
+        if get_all_repo_hash() != self.get_mirror_cache():
+            self.logger.debug('Upstream repo changed! Removing squashfs cache to re-create.')
+            intact = False
+
+        if not intact:
+            self.remove_cache()
+
+        return intact
+
+    @property
+    def installed_packages_in_cache_changed(self):
+        return self.installed_packages_in_cache != self.get_packages()
+
+    def restore_cache(self, chroot_basedir):
+        self.run([
+            'unsquashfs', '-f', '-d', chroot_basedir, self.cache_file_path
+        ])
