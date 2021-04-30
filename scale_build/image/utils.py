34b36f4262d6ced8b97b0bc1dabe10ee398f0364
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-04-30 23:08:14 +0500

    Use functools.cache for retrieving version no

--- scale_build/image/utils.py
+++ scale_build/image/utils.py
@@ -1,17 +1,17 @@
+import functools
 import json
 import os
-import shutil
+import tempfile
 
 from scale_build.exceptions import CallError
 from scale_build.utils.environment import APT_ENV
 from scale_build.utils.run import run
-from scale_build.utils.paths import CHROOT_BASEDIR, TMP_DIR
+from scale_build.utils.paths import CHROOT_BASEDIR
 
 from .manifest import UPDATE_FILE
 
 
 PACKAGE_PATH = os.path.join(CHROOT_BASEDIR, 'packages')
-IMAGE_VERSION = None
 
 
 def run_in_chroot(command, logger=None, exception_message=None, **kwargs):
@@ -21,25 +21,16 @@ def run_in_chroot(command, logger=None, exception_message=None, **kwargs):
     )
 
 
+@functools.cache
 def get_image_version():
-    global IMAGE_VERSION
-    if IMAGE_VERSION:
-        return IMAGE_VERSION
-
     if not os.path.exists(UPDATE_FILE):
         raise CallError(f'{UPDATE_FILE!r} update file does not exist')
 
-    mount_dir = os.path.join(TMP_DIR, 'update_dir')
-    if os.path.exists(mount_dir):
-        shutil.rmtree(mount_dir)
-    os.makedirs(mount_dir)
-    try:
-        run(['mount', UPDATE_FILE, mount_dir, '-t', 'squashfs', '-o', 'loop'])
-        with open(os.path.join(mount_dir, 'manifest.json'), 'r') as f:
-            update_manifest = json.loads(f.read())
-        IMAGE_VERSION = update_manifest['version']
-    finally:
-        run(['umount', '-f', mount_dir])
-        shutil.rmtree(mount_dir)
-
-    return IMAGE_VERSION
+    with tempfile.TemporaryDirectory() as mount_dir:
+        try:
+            run(['mount', UPDATE_FILE, mount_dir, '-t', 'squashfs', '-o', 'loop'])
+            with open(os.path.join(mount_dir, 'manifest.json'), 'r') as f:
+                update_manifest = json.loads(f.read())
+            return update_manifest['version']
+        finally:
+            run(['umount', '-f', mount_dir])
