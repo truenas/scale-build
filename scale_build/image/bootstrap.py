a1d84f8c9a8e61988aa0c61a17812fa28fefa3b3
Waqar Ahmed <waqarahmedjoyia@live.com>
2021-05-05 06:11:46 +0500

    Update update image creation target to utilise new logging method

--- scale_build/image/bootstrap.py
+++ scale_build/image/bootstrap.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import shutil
 
@@ -7,25 +8,25 @@ from scale_build.utils.paths import CHROOT_BASEDIR, PKG_DIR, TMPFS
 from .utils import PACKAGE_PATH
 
 
-def setup_chroot_basedir(bootstrapdir_obj, logger):
+logger = logging.getLogger(__name__)
+
+
+def setup_chroot_basedir(bootstrapdir_obj):
     if os.path.exists(CHROOT_BASEDIR):
         shutil.rmtree(CHROOT_BASEDIR)
     os.makedirs(TMPFS, exist_ok=True)
-    run(
-        ['mount', '-t', 'tmpfs', '-o', 'size=12G', 'tmpfs', TMPFS],
-        logger=logger
-    )
+    run(['mount', '-t', 'tmpfs', '-o', 'size=12G', 'tmpfs', TMPFS])
     bootstrapdir_obj.restore_cache(CHROOT_BASEDIR)
-    run(['mount', 'proc', os.path.join(CHROOT_BASEDIR, 'proc'), '-t', 'proc'], logger=logger)
-    run(['mount', 'sysfs', os.path.join(CHROOT_BASEDIR, 'sys'), '-t', 'sysfs'], logger=logger)
+    run(['mount', 'proc', os.path.join(CHROOT_BASEDIR, 'proc'), '-t', 'proc'])
+    run(['mount', 'sysfs', os.path.join(CHROOT_BASEDIR, 'sys'), '-t', 'sysfs'])
     os.makedirs(PACKAGE_PATH, exist_ok=True)
-    run(['mount', '--bind', PKG_DIR, PACKAGE_PATH], logger=logger)
+    run(['mount', '--bind', PKG_DIR, PACKAGE_PATH])
 
 
 def umount_tmpfs_and_clean_chroot_dir():
     if os.path.exists(CHROOT_BASEDIR):
         shutil.rmtree(CHROOT_BASEDIR)
-    run(['umount', '-f', TMPFS], check=False)
+    run(['umount', '-f', TMPFS], check=False, log=False)
 
 
 def umount_chroot_basedir():
@@ -34,7 +35,7 @@ def umount_chroot_basedir():
         ['umount', '-f', os.path.join(CHROOT_BASEDIR, 'proc')],
         ['umount', '-f', os.path.join(CHROOT_BASEDIR, 'sys')],
     ):
-        run(command, check=False)
+        run(command, check=False, log=False)
 
 
 def clean_mounts():
