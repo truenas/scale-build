539e3eb1d14b9d5d2016e4d5960de5fa42be026b
Andrew Walker <awalker@ixsystems.com>
2022-02-02 12:39:46 -0500

    remove try/except

--- truenas_install/__main__.py
+++ truenas_install/__main__.py
@@ -83,10 +83,7 @@ def enable_user_services(root, old_root):
         ]
 
     if systemd_units:
-        try:
-            run_command(["chroot", root, "systemctl", "enable"] + systemd_units, check=False)
-        except Exception:
-            pass
+        run_command(["chroot", root, "systemctl", "enable"] + systemd_units, check=False)
 
 
 def install_grub_freebsd(input, manifest, pool_name, dataset_name, disks):
