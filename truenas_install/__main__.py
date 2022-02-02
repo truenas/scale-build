c2a204c6e6ea768a66ed14decbde2b9398595e70
Andrew Walker <awalker@ixsystems.com>
2022-02-02 12:28:55 -0500

    Set check=false

--- truenas_install/__main__.py
+++ truenas_install/__main__.py
@@ -84,7 +84,7 @@ def enable_user_services(root, old_root):
 
     if systemd_units:
         try:
-            run_command(["chroot", root, "systemctl", "enable"] + systemd_units)
+            run_command(["chroot", root, "systemctl", "enable"] + systemd_units, check=False)
         except Exception:
             pass
 
