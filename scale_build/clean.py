f23ccd8bfa2c6a517754c4e1c385175318e3f593
caleb <yocalebo@gmail.com>
2023-10-17 11:30:28 -0400

    ignore more exceptions in complete_cleanup()
    (cherry picked from commit 12b7e0dab945468e09797c235599387bd7b87363)

--- scale_build/clean.py
+++ scale_build/clean.py
@@ -21,7 +21,26 @@ def clean_packages():
 
 
 def complete_cleanup():
-    for path in filter(os.path.exists, (LOG_DIR, SOURCES_DIR, TMP_DIR)):
-        shutil.rmtree(path)
+    for path in (LOG_DIR, SOURCES_DIR, TMP_DIR):
+        try:
+            shutil.rmtree(path)
+        except (FileNotFoundError, NotADirectoryError):
+            # these shouldn't happen but they're explicitly
+            # ignored so a comment can be made and you are
+            # able to gather context
+            continue
+        except OSError:
+            # we use a bunch of overlayfs mounts (using tmpfs) for
+            # each package that we build. if the build is interrupted
+            # for whatever reason (someone kills job in jenkins) then
+            # OSError can be raised with errno 66 (directory not empty)
+            # for example because the `TMP_DIR` can have a sub-directory
+            # that points back to an overlayfs mountpoint that has not
+            # been umounted (because build process was interrupted)
+            # In this case, we can safely ignore the errors and move on
+            # because the next time our build process is kicked off, each
+            # package will call `delete_overlayfs()` which will clean this
+            # up for us
+            continue
 
     logger.debug('Removed %s, %s, and %s directories', LOG_DIR, SOURCES_DIR, TMP_DIR)
