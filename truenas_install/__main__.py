b6b350a73b92b134a31e8d12d38e8066590786ea
themylogin <themylogin@gmail.com>
2024-02-07 19:02:12 +0100

    PEP8

--- truenas_install/__main__.py
+++ truenas_install/__main__.py
@@ -354,11 +354,11 @@ def main():
     old_root = input.get("old_root", None)
 
     if IS_FREEBSD:
-        write_error("""
-            Migrating TrueNAS CORE to TrueNAS SCALE 24.04 (or later) using update file upload is not supported. Please
-            migrate with the latest 23.10 release update file or back up the TrueNAS configuration, perform a fresh install,
-            and restore from the configuration backup.
-        """)
+        write_error(
+            "Migrating TrueNAS CORE to TrueNAS SCALE 24.04 (or later) using update file upload is not supported. "
+            "Please migrate with the latest 23.10 release update file or back up the TrueNAS configuration, perform a "
+            "fresh install, and restore from the configuration backup."
+        )
         sys.exit(2)
 
     if input.get("precheck"):
