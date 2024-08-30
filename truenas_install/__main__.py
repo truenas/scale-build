6f41f48b13bf96df7440f150c95909be3d395de3
themylogin <themylogin@gmail.com>
2024-08-30 16:25:51 +0200

    Do not use pending uploaded database on the new system
    (cherry picked from commit 5db2017951d10156d96e121a50cce5f4e4c9b8f4)

--- truenas_install/__main__.py
+++ truenas_install/__main__.py
@@ -357,6 +357,7 @@ def main():
                     "data/factory-v1.db",
                     "data/manifest.json",
                     "data/sentinels",
+                    "data/uploaded.db",
                 ]
 
                 if "data" in cloned_datasets:
