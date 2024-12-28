18d246f435f848e14464f0df6715ab35d588ba1d
Caleb <yocalebo@gmail.com>
2024-12-28 13:34:09 -0500

    read from proc/meminfo in utils/system.py

--- scale_build/utils/system.py
+++ scale_build/utils/system.py
@@ -1,8 +1,12 @@
-import psutil
+from functools import cache
+
+REQUIRED_RAM_GB = 16 * (1024 ** 3)
+
+__all__ = ("has_low_ram",)
 
 
-REQUIRED_RAM = 16  # GB
-
-
+@cache
 def has_low_ram():
-    return psutil.virtual_memory().total < REQUIRED_RAM * 1024 * 1024 * 1024
+    with open('/proc/meminfo') as f:
+        for line in filter(lambda x: 'MemTotal' in x, f):
+            return int(line.split()[1]) * 1024 < REQUIRED_RAM_GB
